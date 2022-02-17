5c49939f8b80edab30420183a7b2af8fdae47a3e
Will Arevalo <willarevalo.developer@gmail.com>
2022-02-17 10:50:56 -0500

    Add feature retrieve all jobs

--- backend/apis/version1/route_jobs.py
+++ backend/apis/version1/route_jobs.py
@@ -1,13 +1,14 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
+from typing import List
 
 from schemas.jobs import ShowJob, JobCreate
 from db.session import get_db
-from db.repository.jobs import create_new_job, retrieve_job
+from db.repository.jobs import create_new_job, retrieve_job, list_jobs
 
 router = APIRouter()
 
-@router.post("/create-job", response_model=ShowJob)
+@router.post("/create-job/", response_model=ShowJob)
 def create_job(job: JobCreate, db:Session=Depends(get_db)):
     owner_id=1
     job = create_new_job(job=job, db=db, owner_id=owner_id)
@@ -22,3 +23,8 @@ def retrieve_job_by_id(id:int, db:Session=Depends(get_db)):
             status_code=status.HTTP_404_NOT_FOUND
             ,detail= f'Job with id {id} does not exist')
     return job
+
+@router.get("/all/", response_model=List[ShowJob])
+def retrieve_all_jobs(db:Session = Depends(get_db)):
+    jobs = list_jobs(db=db)
+    return jobs
