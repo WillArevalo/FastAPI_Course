dd96fc66cd69329c90d54a6592688109d019562e
Will Arevalo <willarevalo.developer@gmail.com>
2022-02-17 12:53:04 -0500

    Add new feature for update a job by id

--- backend/apis/version1/route_jobs.py
+++ backend/apis/version1/route_jobs.py
@@ -4,7 +4,7 @@ from typing import List
 
 from schemas.jobs import ShowJob, JobCreate
 from db.session import get_db
-from db.repository.jobs import create_new_job, retrieve_job, list_jobs
+from db.repository.jobs import create_new_job, retrieve_job, list_jobs, update_job_by_id
 
 router = APIRouter()
 
@@ -28,3 +28,13 @@ def retrieve_job_by_id(id:int, db:Session=Depends(get_db)):
 def retrieve_all_jobs(db:Session = Depends(get_db)):
     jobs = list_jobs(db=db)
     return jobs
+
+@router.put("/update/{id}")
+def update_job(id:int, job: JobCreate, db:Session = Depends(get_db)):
+    owner_id = 1 # in this moment this var is hardcoded but in nexts session we will implement a auth system and we change this
+    message = update_job_by_id(id=id, job=job, db=db, owner_id=owner_id)
+    if not message: #result of searching and not finding the job is 0
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+        detail=f"Job with id {id} does not exist")
+    return {"deatil": f"Successfully update job with id {id}"}
+        
