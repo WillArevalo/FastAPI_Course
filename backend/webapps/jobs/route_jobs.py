3ca8217194264bb32a966f7efcf64897f0bc96ac
Will Arevalo <willarevalo.developer@gmail.com>
2022-03-04 21:25:29 -0500

    created a homepage without draw in schemas

---
+++ backend/webapps/jobs/route_jobs.py
@@ -0,0 +1,18 @@
+from fastapi import APIRouter
+from fastapi import Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+
+templates = Jinja2Templates(directory="templates")
+router = APIRouter(include_in_schema=False)
+
+
+@router.get("/")
+def home(request:Request):
+    # print(dir(request))
+    return templates.TemplateResponse(
+        name="jobs/homepage.html", 
+        context={'request':request})
+
+
