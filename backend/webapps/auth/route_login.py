1ca9434452cb7c4ceda843d50647150c71275e85
Will Arevalo <willarevalo.developer@gmail.com>
2022-06-18 11:26:36 -0500

    Add bussiness logic for login

--- backend/webapps/auth/route_login.py
+++ backend/webapps/auth/route_login.py
@@ -1,5 +1,10 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Depends, HTTPException, Request
 from fastapi.templating import Jinja2Templates
+from sqlalchemy.orm import Session
+
+from apis.version1.route_login import login_for_access_token
+from webapps.auth.forms import LoginForm
+from db.session import get_db
 
 
 templates = Jinja2Templates(directory="templates")
@@ -12,4 +17,24 @@ def login(request : Request):
         {"request": request}
     )
 
-
+@router.post('/login/')
+async def login(request : Request, db : Session = Depends(get_db)):
+    form = LoginForm(request)
+    await form.load_data()
+    if await form.is_valid():
+        try:
+            form.__dict__.update(msg="Login Successful")
+            response = templates.TemplateResponse(
+                "auth/login.html", 
+                form.__dict__)
+            login_for_access_token(
+                response=response,
+                form_data=form,
+                db=db)
+            return response
+        except HTTPException:
+            form.__dict__.update(msg="")
+            form.__dict__.get("errors").append("Incorrect email and password")
+            return templates.TemplateResponse(
+                "auth/login.html", 
+                form.__dict__)
